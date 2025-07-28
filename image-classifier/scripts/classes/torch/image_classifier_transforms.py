498a2ed5cfb59f1f75eb7fd79fc5eb1a9df00e76
FrancescoNicotra <francesco.nicotra23@gmail.com>
2025-07-28 23:16:29 +0200

    initial commit

--- image-classifier/scripts/classes/torch/image_classifier_transforms.py
+++ image-classifier/scripts/classes/torch/image_classifier_transforms.py
@@ -7,6 +7,7 @@ class ImageClassifierTransforms:
         self.transform = v2.Compose([
             v2.RandomResizedCrop(size=(224, 224), antialias=True),
             v2.RandomHorizontalFlip(p=0.5),
+            v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
