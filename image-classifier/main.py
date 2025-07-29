6321c29128c2bd07650ce8f428c56c05204cf039
Francesco Nicotra <francesco.nicotra@thelevelgroup.com>
2025-07-29 13:20:08 +0200

    Class improvements

--- image-classifier/main.py
+++ image-classifier/main.py
@@ -5,7 +5,7 @@ from torchvision.datasets import CIFAR10
 
 if __name__ == "__main__":
     plt.rcParams["savefig.bbox"] = "tight"
-    transforms = ImageClassifierTransforms()
+    transforms = ImageClassifierTransforms([224,224],True, (0.08,1.0), 0.5, True, dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), True)
     datasets = CIFAR10(root="./models/cifar10", download=True, train=True, transform=transforms)
     random_index = torch.randint(0, len(datasets), (1,)).item()
     img, label = datasets[int(random_index)]
--- image-classifier/scripts/classes/torch/image_classifier_transforms.py
+++ image-classifier/scripts/classes/torch/image_classifier_transforms.py
@@ -1,16 +1,38 @@
 import torch
 from torchvision.transforms import v2
+from typing import Tuple, Sequence
+from typing import Optional, Dict, Sequence
 
-#define class to create torch transforms for image classification
 class ImageClassifierTransforms:
-    def __init__(self) -> None:
-        self.transform = v2.Compose([
-            v2.RandomResizedCrop(size=(224, 224), antialias=True),
-            v2.RandomHorizontalFlip(p=0.5),
-            v2.ToImage(),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+    def __init__(
+        self,
+        size: Sequence[int],
+        antialias: bool,
+        scale: Tuple[float, float],
+        p: float = 0.5,
+        normalize: bool = True,
+        normalizeObj: Optional[Dict[str, Sequence[float]]] = None,
+        toImage: bool = True
+    ) -> None:
+        transforms = [
+            v2.RandomResizedCrop(size=size, antialias=antialias, scale=scale),
+            v2.RandomHorizontalFlip(p=p),
+        ]
+
+        if toImage:
+            transforms.append(v2.ToImage())
+
+        transforms.append(v2.ToDtype(torch.float32, scale=True))
+
+        if normalize:
+            if normalizeObj is None:
+                normalizeObj = {
+                    "mean": [0.485, 0.456, 0.406],
+                    "std": [0.229, 0.224, 0.225]
+                }
+            transforms.append(v2.Normalize(**normalizeObj))
+
+        self.transform = v2.Compose(transforms)
 
     def __call__(self, img: torch.Tensor) -> torch.Tensor:
         return self.transform(img)
